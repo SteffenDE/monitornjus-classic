ffb7045810e25804fea0329fd0f96eaf9424be64
steffend <Steffen@SDMac.local>
2015-06-23 17:51:26 +0200

    tried to reduce CPU usage again...

--- admin/widgets.py
+++ admin/widgets.py
@@ -99,7 +99,7 @@ try:
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Admin-Link", "align"))+"""\' name="adminlinkalign" id="adminlinkalign" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Admin-Link", "align")+"""\' name="adminlinkalign" id="adminlinkalign" type="text"/>
 												<label for="adminlinkalign">Vertikaler Abstand</label>
 											</div>
 										</div>
@@ -111,7 +111,7 @@ try:
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Admin-Link", "margin"))+"""\' name="adminlinkmargin" id="adminlinkmargin" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Admin-Link", "margin")+"""\' name="adminlinkmargin" id="adminlinkmargin" type="text"/>
 												<label for="adminlinkmargin">Horizontaler Abstand</label>
 											</div>
 										</div>
@@ -124,11 +124,11 @@ try:
 						<div class="card white darken-1">
 							<div class="card-content white-text">
 								<span class="card-title """+colors.color+"""-text text-darken-2">Uhr</span><br><br>
-								<input type="checkbox" name="uhraktiv" id="uhraktiv" """+checkvalues.uhraktiv+"""/>
+								<input type="checkbox" name="uhraktiv" id="uhraktiv" """+checkvalues.widgaktiv("Uhr")+"""/>
 								<label for="uhraktiv">Uhr aktiviert</label>
 								<div class="row">
 									<div class="input-field col s12">
-										<input style="color:black;" placeholder=\""""+checkvalues.uhrlink+"""\" name="uhrlink" id="uhrlink" type="text"/>
+										<input style="color:black;" placeholder=\""""+common.getwidgetinfo("Uhr", "URL")+"""\" name="uhrlink" id="uhrlink" type="text"/>
 										<label for="uhrlink">Uhr-URL</label>
 									</div>
 								</div>
@@ -136,7 +136,7 @@ try:
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Uhr", "height"))+"""\' name="uhrheight" id="uhrheight" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Uhr", "height")+"""\' name="uhrheight" id="uhrheight" type="text"/>
 												<label for="uhrheight">Uhr-H&ouml;he</label>
 											</div>
 										</div>
@@ -144,31 +144,31 @@ try:
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Uhr", "width"))+"""\' name="uhrwidth" id="uhrwidth" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Uhr", "width")+"""\' name="uhrwidth" id="uhrwidth" type="text"/>
 												<label for="uhrwidth">Uhr-Breite</label>
 											</div>
 										</div>
 									</div>
 									<div class="col s6">
-										<select name="dropdown_uhr_valign">"""+checkvalues.uhrvalign+"""
+										<select name="dropdown_uhr_valign">"""+checkvalues.valign("Uhr", "valign")+"""
 										</select>
 									</div>
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Uhr", "align"))+"""\' name="uhralign" id="uhralign" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Uhr", "align")+"""\' name="uhralign" id="uhralign" type="text"/>
 												<label for="uhralign">Vertikaler Abstand</label>
 											</div>
 										</div>
 									</div>
 									<div class="col s6">
-										<select name="dropdown_uhr_vmargin">"""+checkvalues.uhrvmargin+"""
+										<select name="dropdown_uhr_vmargin">"""+checkvalues.valign("Uhr", "vmargin")+"""
 										</select>
 									</div>
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Uhr", "margin"))+"""\' name="uhrmargin" id="uhrmargin" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Uhr", "margin")+"""\' name="uhrmargin" id="uhrmargin" type="text"/>
 												<label for="uhrmargin">Horizontaler Abstand</label>
 											</div>
 										</div>
@@ -181,11 +181,11 @@ try:
 						<div class="card white darken-1">
 							<div class="card-content white-text">
 								<span class="card-title """+colors.color+"""-text text-darken-2">Freies Widget</span><br><br>
-								<input type="checkbox" name="widgetaktiv" id="widgetaktiv" """+checkvalues.widgetaktiv+"""/>
+								<input type="checkbox" name="widgetaktiv" id="widgetaktiv" """+checkvalues.widgaktiv("Freies-Widget")+"""/>
 								<label for="widgetaktiv">Widget aktiviert</label>
 								<div class="row">
 									<div class="input-field col s12">
-										<textarea style="color:black;" name="widgetlink" id="widgetlink" class="materialize-textarea">"""+checkvalues.widgetlink+"""</textarea>
+										<textarea style="color:black;" name="widgetlink" id="widgetlink" class="materialize-textarea">"""+common.getwidgetinfo("Freies-Widget", "URL")+"""</textarea>
 										<label for="widgetlink">Widget-URL</label>
 									</div>
 								</div>
@@ -193,7 +193,7 @@ try:
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Freies-Widget", "height"))+"""\' name="widgetheight" id="widgetheight" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Freies-Widget", "height")+"""\' name="widgetheight" id="widgetheight" type="text"/>
 												<label for="widgetheight">Widget-H&ouml;he</label>
 											</div>
 										</div>
@@ -201,31 +201,31 @@ try:
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Freies-Widget", "width"))+"""\' name="widgetwidth" id="widgetwidth" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Freies-Widget", "width")+"""\' name="widgetwidth" id="widgetwidth" type="text"/>
 												<label for="widgetwidth">Widget-Breite</label>
 											</div>
 										</div>
 									</div>
 									<div class="col s6">
-										<select name="dropdown_widget_valign">"""+checkvalues.widgetvalign+"""
+										<select name="dropdown_widget_valign">"""+checkvalues.valign("Freies-Widget", "valign")+"""
 										</select>
 									</div>
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Freies-Widget", "align"))+"""\' name="widgetalign" id="widgetalign" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Freies-Widget", "align")+"""\' name="widgetalign" id="widgetalign" type="text"/>
 												<label for="widgetalign">Vertikaler Abstand</label>
 											</div>
 										</div>
 									</div>
 									<div class="col s6">
-										<select name="dropdown_widget_vmargin">"""+checkvalues.widgetvmargin+"""
+										<select name="dropdown_widget_vmargin">"""+checkvalues.valign("Freies-Widget", "vmargin")+"""
 										</select>
 									</div>
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+str(common.getwidgetinfo("Freies-Widget", "margin"))+"""\' name="widgetmargin" id="widgetmargin" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Freies-Widget", "margin")+"""\' name="widgetmargin" id="widgetmargin" type="text"/>
 												<label for="widgetmargin">Horizontaler Abstand</label>
 											</div>
 										</div>
@@ -238,11 +238,11 @@ try:
 						<div class="card white darken-1">
 							<div class="card-content white-text">
 								<span class="card-title """+colors.color+"""-text text-darken-2">Logo</span><br><br>
-								<input type="checkbox" name="logoaktiv" id="logoaktiv" """+checkvalues.logoaktiv+"""/>
+								<input type="checkbox" name="logoaktiv" id="logoaktiv" """+checkvalues.widgaktiv("Logo")+"""/>
 								<label for="logoaktiv">Logo aktiviert</label>
 								<div class="row">
 									<div class="input-field col s12">
-										<input style="color:black;" placeholder=\""""+checkvalues.logolink+"""\" name="logolink" id="logolink" type="text"/>
+										<input style="color:black;" placeholder=\""""+common.getwidgetinfo("Logo", "URL")+"""\" name="logolink" id="logolink" type="text"/>
 										<label for="logolink">Logo-URL</label>
 									</div>
 								</div>
@@ -250,7 +250,7 @@ try:
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+checkvalues.logoheight+"""\' name="logoheight" id="logoheight" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Logo", "height")+"""\' name="logoheight" id="logoheight" type="text"/>
 												<label for="logoheight">Logo-H&ouml;he</label>
 											</div>
 										</div>
@@ -258,31 +258,31 @@ try:
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+checkvalues.logowidth+"""\' name="logowidth" id="logowidth" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Logo", "width")+"""\' name="logowidth" id="logowidth" type="text"/>
 												<label for="logowidth">Logo-Breite</label>
 											</div>
 										</div>
 									</div>
 									<div class="col s6">
-										<select name="dropdown_logo_valign">"""+checkvalues.logovalign+"""
+										<select name="dropdown_logo_valign">"""+checkvalues.valign("Logo", "valign")+"""
 										</select>
 									</div>
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+checkvalues.logoalign+"""\' name="logoalign" id="logoalign" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Logo", "align")+"""\' name="logoalign" id="logoalign" type="text"/>
 												<label for="logoalign">Vertikaler Abstand</label>
 											</div>
 										</div>
 									</div>
 									<div class="col s6">
-										<select name="dropdown_logo_vmargin">"""+checkvalues.logovmargin+"""
+										<select name="dropdown_logo_vmargin">"""+checkvalues.valign("Logo", "vmargin")+"""
 										</select>
 									</div>
 									<div class="col s6">
 										<div class="row">
 											<div class="input-field col s12">
-												<input style="color:black;" placeholder=\'"""+checkvalues.logomargin+"""\' name="logomargin" id="logomargin" type="text"/>
+												<input style="color:black;" placeholder=\'"""+common.getwidgetinfo("Logo", "margin")+"""\' name="logomargin" id="logomargin" type="text"/>
 												<label for="logomargin">Horizontaler Abstand</label>
 											</div>
 										</div>
